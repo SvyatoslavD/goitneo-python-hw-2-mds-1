79d9a32902a2cd3239c02350c61fc8e1ffcb9b29
Svyatoslav Dzyadevych <svyatoslav.dzyadevych@gmail.com>
2023-10-22 21:21:33 +0300

    Added code that implements task #2 - implemented contacts DB using OOP
    Signed-off-by: Svyatoslav Dzyadevych <svyatoslav.dzyadevych@gmail.com>

---
+++ task_2.py
@@ -0,0 +1,109 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Name cannot be empty")
+        super().__init__(value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        Phone.validate_phone(value)
+        super().__init__(value)
+
+    @classmethod
+    def validate_phone(cls, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Phone number must be 10 digits")
+
+    def phone_set(self, value):
+        Phone.validate_phone(value)
+        self.value = value
+
+    def phone_get(self):
+        return self.value
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.phone_get() != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone.phone_get() == old_phone:
+                phone.phone_set(new_phone)
+                break
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.phone_get() == phone:
+                return p
+        return None
+
+    def __str__(self):
+        phones_str = "; ".join(str(phone) for phone in self.phones)
+        return f"Contact name: {self.name}, phones: {phones_str}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+
+if __name__ == "__main__":
+    # Створення нової адресної книги
+    book = AddressBook()
+
+    # Створення запису для John
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+
+    # Додавання запису John до адресної книги
+    book.add_record(john_record)
+
+    # Створення та додавання нового запису для Jane
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    # Виведення всіх записів у книзі
+    for name, record in book.data.items():
+        print(record)
+
+    # Знаходження та редагування телефону для John
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+    # Пошук конкретного телефону у записі John
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+    # Видалення запису Jane
+    book.delete("Jane")
